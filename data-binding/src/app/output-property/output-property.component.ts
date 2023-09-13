9c47450cdf3a7b6ba92788f01a7faa06df5397c5
diego-bm <dborgesmoraes@outlook.com>
2023-09-13 09:45:16 -0300

    Aula 16: acesso ao DOM e ao Template com ViewChild
    Conteúdo e anotações da aula implementados com sucesso.
    
    OBS: a aula terminou com um bug no console log do evento de mudança do data-binding component. Agora ele está sempre emitindo o mesmo valor. Esse bug também ocorreu no projeto da Loiane, então, irei assumir que será tratado em outra aula. Se eu notar que foi ignorado, irei tentar resolver por mim mesmo.

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 // Assim como o Input, os outputs também podem ser declarados nos metadados
 // do componente. Entretanto, fica mais fácil de ler e localizar se usarmos
@@ -15,13 +15,29 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  // O "!" serve para dizer ao Typescript que tenho certeza que essa variável
+  // terá um valor atribuído antes de ser usado. Se não estivesse usando o ViewChild,
+  // o certo seria inicializar um objeto vazio no construtor para que não ocorra
+  // erros com null ou undefined. Entretanto, como eu já irei atribuir um elemento
+  // HTML a essa variável por conta do ViewChild, eu já estou garantindo que o
+  // objeto será preenchido. Em teoria, eu até posso inicializar a variável no
+  // construtor, mas ela já será sobrescrita pelo ViewChild. Então, para deixar
+  // mais fácil de ler e seguir as boas práticas, é melhor usar o operador de afirmação
+  // não nula (!).
+  // -----
+  // Também utilizamos o HTMLElement para saber qual o tipo do nosso elemento,
+  // dando um console.log nele. Descobrimos que é um ElementRef.
+  // @ViewChild('campoInput') campoValorInput!: HTMLElement;
+
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
+
   incrementa(){
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa(){
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
