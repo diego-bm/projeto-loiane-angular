803029f8ed9f4b9e6df3606e4f921625a51e8fc6
diego-bm <dborgesmoraes@outlook.com>
2023-09-17 17:06:31 -0300

    Aula 27: Diretivas: ngFor
    Código da aula implementado com sucesso.
    
    OBS: a aula de ngIf foi pulada alguns commits atrás pois eu já entendo desse conteúdo.

---
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.cy.ts
@@ -0,0 +1,7 @@
+import { DiretivaNgforComponent } from './diretiva-ngfor.component'
+
+describe('DiretivaNgforComponent', () => {
+  it('should mount', () => {
+    cy.mount(DiretivaNgforComponent)
+  })
+})
