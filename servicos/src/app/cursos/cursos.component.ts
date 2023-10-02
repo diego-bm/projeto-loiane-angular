62e8195df273505ffeb885e1f9af6205d65f756e
diego-bm <dborgesmoraes@outlook.com>
2023-10-02 11:53:16 -0300

    Aula 38 e 39: Serviços
    Código das aulas implementado com sucesso. Me esqueci de fazer commit delas.

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from './cursos.service'
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent {
+  cursos: string[] = [];
+  // cursosService: CursosService;
+
+  constructor( private cursosService: CursosService) {
+    // this.cursosService = new CursosService;
+    // this.cursosService = _cursosService;
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
+  }
+}
