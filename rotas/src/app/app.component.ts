2e706f92df44b20e215ddd72a38c1bf0cb5f89e7
diego-bm <dborgesmoraes@outlook.com>
2024-02-26 15:08:44 -0300

    Aula 51: Rotas: RouterLink: Definindo rotas no template
    Código da aula não foi aplicado com êxito.
    
    O código que remete aos RouterLinks, o essêncial da aula, deu certo. Entretanto, o Materialize não funcionou (de novo) e não encontro um suporte decente para esse framework de estilização. Com isso em mente, eu decidi começar a utilizar o Angular Material toda vez que o Materialize for utilizado nas aulas. É aprendizado novo e mexe com algo que já mexi enquanto trabalhava na Moura.
    
    Meus agradecimentos vão para esse site: https://dev.to/davidihl/how-to-create-a-responsive-sidebar-and-mini-navigation-with-material-angular-o5l#:~:text=How%20to%20create%20a%20responsive%20sidebar%20and%20mini,...%207%20Step%207%3A%20Collapsing%20the%20navigation%20 -->
    
    Várias anotações também foram adicionadas ao código com pequenos aprendizados e dúvidas que tive durante minhas pesquisas.

--- rotas/src/app/app.component.ts
+++ rotas/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  ViewChild
+} from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rotas';
+  @ViewChild(MatSidenav)
+  // O exclamação é chamado de assert, ele basicamente assegura o transpilador
+  // de que você tem certeza que essa variável não será nula ou indefinida.
+  // A "assertividade" do nome vem do desenvolvedor, então eu imagino que isso
+  // deva ser utilizado com cuidado.
+  sidenav!: MatSidenav;
+  isMobile: boolean = true;
+  isCollapsed: boolean = true;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+  protected toggleMenu() {
+    if(this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false; // O menu nunca deve ser colapsado em mobile
+    } else {
+      this.sidenav.open(); // Em desktops e tablets, o menu nunca deve ser totalmente fechado
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule  } from '@angular/material/icon';
+import { MatSidenavModule  } from '@angular/material/sidenav';
+import { MatToolbarModule  } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +22,13 @@ import { CursosComponent } from './cursos/cursos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
