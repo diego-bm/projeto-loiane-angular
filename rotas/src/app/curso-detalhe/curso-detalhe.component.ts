fdb32ffdc2c27a253b8f1ba899ac2ce39a858d91
diego-bm <dborgesmoraes@outlook.com>
2024-03-04 14:11:19 -0300

    Aula 54: Rotas: Escutando mudanças nos parâmetros de roteamento
    Código da aula implementado com sucesso.
    
    Só por que eu falei do problema da atualização do Id, foi exatamente o que tratamos na aula de hoje.

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -7,14 +8,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./curso-detalhe.component.scss']
 })
 export class CursoDetalheComponent {
-
   id: string = '';
+  inscricao: Subscription = new Subscription;
 
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
-
-    console.log(route);
-    
+    // this.id = this.route.snapshot.params['id'];
+    // console.log(this.route);
   }
 
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }
