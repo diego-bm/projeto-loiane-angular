35c63c626c95a5cc4b1d8bb179db86f7e4e5fea3
diego-bm <dborgesmoraes@outlook.com>
2024-04-01 16:48:51 -0300

    Criação do projeto forms e Bootstrap
    Segui um tutorial do blog da Loiane:
    https://loiane.com/2017/08/how-to-add-bootstrap-to-an-angular-cli-project/

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
