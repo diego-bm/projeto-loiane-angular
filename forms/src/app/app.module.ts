cfa11161f836f91d7e47d1be42fdad0cd78f0494
diego-bm <dborgesmoraes@outlook.com>
2024-04-15 13:49:37 -0300

    Aula 73: Formulários - Criando o projeto inicial com Bootstrap 3
    Código da aula implementado com observações.
    
    FINALMENTE consegui fazer o Bootstrap funcionar.
    Agradecimentos vão para esse link:
    https://loiane.com/2017/08/how-to-add-bootstrap-to-an-angular-cli-project/
    Que também é da Loiane! Olha só.

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule,
