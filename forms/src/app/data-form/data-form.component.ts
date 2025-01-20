a887b39e0de72b2ba0767038dcb7a68564394fc6
diego-bm <dborgesmoraes@outlook.com>
2025-01-20 16:56:38 -0300

    Aula 89: Formulários reativos: Configuração (Módulo e Componente)
    Código da aula implementado com observações.
    
    - O Angular não permite mais variáveis não inicializadas, então, por enquanto, o site não está compilável.

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-form.component.css']
 })
 export class DataFormComponent {
+
+  formulario: FormGroup;
+
+  constructor(){
+    
+  }
 
 }
