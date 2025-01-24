d9dd25520cd7ff7ed9f02c7dd95ed759b4ed0a37
diego-bm <dborgesmoraes@outlook.com>
2025-01-24 18:58:00 -0300

    Aula 106: FR: Checkbox Toggle
    Código da aula implementado com observações.
    
    - Tive que fazer ajustes manuais no CSS, como sempre.

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -68,7 +68,8 @@ export class DataFormComponent {
 
       cargo: [null],
       tecnologias: [null],
-      newsletter: ['s']
+      newsletter: ['s'],
+      termos: [null, Validators.pattern('true')]
     });
     // Exemplos de Validators: [Validators.required, Validators.minLength(3), Validators.maxLength(20)]
 
