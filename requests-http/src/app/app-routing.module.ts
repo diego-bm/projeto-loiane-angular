4a06ba9c70d7b372155472f8fd74027a2878fc2b
diego-bm <dborgesmoraes@outlook.com>
2025-02-17 18:43:54 -0300

    Aula 135: Upload de Arquivo: Formulário Upload com Bootstrap 4 (5)
    Código da aula implementado com observações.
    
    - Foi usado o Bootstrap 5, pois é o mais atualizado;
    - Não foi feita a lógica implementada na aula, pois não é necessária para o upload de apenas 1 arquivo e a forma como o Bootstrap 5 mostra os arquivos em um file input multiple é mais elegante aos meus olhos em seu estado natural.

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: 'cursos'
+    // Durante desenvolvimento LOCAL, é legal trocar esse redirect para a
+    // página que você está trabalhando.
+    path: '', pathMatch: 'full', redirectTo: 'upload'
   },
   {
     path: 'cursos',
     loadChildren: () => import('./cursos/cursos.module').then(x => x.CursosModule)
+  },
+  {
+    path: 'upload',
+    loadChildren: () => import('./upload-file/upload-file.module').then(x => x.UploadFileModule)
   },
   {
     path: 'rxjs-poc',
