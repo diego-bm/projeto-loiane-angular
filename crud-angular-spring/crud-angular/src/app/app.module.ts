7f2a41b9ff4deef65c33cc49b8d11a0c52207c55
diego-bm <dborgesmoraes@outlook.com>
2025-04-01 19:47:19 -0300

    CRUD-A+S: Aula 3 INCOMPLETO 2
    - Segui testando diferentes coisas para aplicar CSS customizado nos componentes do Material. Ainda sem sucesso;
    - Soluções online não funcionam;
    - Documentação oficial não funciona e nem especifica nada para as toolbars;
    - Irei tentar entrar no Discord oficial do Angular Material para ver se consigo qualquer ajuda.

--- crud-angular-spring/crud-angular/src/app/app.module.ts
+++ crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
