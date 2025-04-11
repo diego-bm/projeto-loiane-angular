8e6d011137deab72718a7d33003c851c6a4d75bb
diego-bm <dborgesmoraes@outlook.com>
2025-04-11 16:08:47 -0300

    CRUD-A+S: Aula 4: Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading
    Código da aula implementado com sucesso.

---
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  standalone: false,
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+
+}
